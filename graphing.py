b41b781610f00da0ad1b471a94dc73a7a6dff38b
matudaira(iwiLabMax) <matsu222212@gmail.com>
2022-12-22 20:49:30 +0900

    feat:全てのデータをグラフで表示するように実装

--- graphing.py
+++ graphing.py
@@ -5,5 +5,13 @@ import json
 
 data_json = requests.get("http://172.16.2.9:5000/get").json()
 
-for (time,value) in json.loads(str(data_json)).items():
-    print(time,value)
+formatted_dic = {float(data_str): mag for data_str,mag in data_json.items()}
+
+formatted_dic = dict(( (data,mag) for data,mag in sorted(formatted_dic.items())))
+
+x = formatted_dic.keys()
+
+y = formatted_dic.values()
+
+plt.plot(x, y)
+plt.show()
